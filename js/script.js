4aeb0b5fe3a01e0ea309723a7bebe303f2c2bc4b
Ashok Anumandla <ashokanumandla100@gmail.com>
2021-03-22 10:22:47 +0530

    secondary navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const navLinks = document.querySelectorAll('nav ul li a');
+const mainNavLinks = document.querySelectorAll('.main-nav-wrapper nav a');
 
-
-const addRemoveActiveClass = (event) => {
-    navLinks.forEach(link => {
+const addRemoveActiveClass = (list, event) => {
+    list.forEach(link => {
         link.classList.remove('active');
     })
     event.target.classList.add('active');
@@ -10,6 +10,12 @@ const addRemoveActiveClass = (event) => {
 
 navLinks.forEach(link => {
     link.addEventListener('click', event => {
-        addRemoveActiveClass(event);
+        addRemoveActiveClass(navLinks, event);
+    });
+});
+
+mainNavLinks.forEach(link => {
+    link.addEventListener('click', event => {
+        addRemoveActiveClass(mainNavLinks, event);
     });
 });
