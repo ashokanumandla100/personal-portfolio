58c8b48c6333afc5043bddf0435bcf37d892db24
Ashok Anumandla <ashokanumandla100@gmail.com>
2021-03-22 12:42:44 +0530

    small changes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 const navLinks = document.querySelectorAll('nav ul li a');
 const mainNavLinks = document.querySelectorAll('.main-nav-wrapper nav a');
+const topItem = document.querySelector('.scroll-to-top a');
+const exp = document.querySelector('.exp');
 
 const addRemoveActiveClass = (list, event) => {
     list.forEach(link => {
         link.classList.remove('active');
-    })
+    });
     event.target.classList.add('active');
 }
 
@@ -18,4 +20,11 @@ mainNavLinks.forEach(link => {
     link.addEventListener('click', event => {
         addRemoveActiveClass(mainNavLinks, event);
     });
+});
+
+topItem.addEventListener('click', function() {
+    mainNavLinks.forEach(link => {
+        link.classList.remove('active');
+    });
+    exp.classList.add('active');
 });
